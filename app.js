69de7497ed1b06160e0592ce50c79273412e5dd1
andrea23js <vazquezsanchesandrea@gmail.com>
2025-03-17 19:05:40 -0500

    función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 let amigos = [];
 
+function agregarAmigo() {
+  const input = document.getElementById("amigo").value.trim();
+  if (input !== "") {
+      amigos.push(input);
+      document.getElementById("amigo").value = "";
+      actualizarLista();
+  } else {
+      alert("Por favor, inserte un nombre");
+  }
+}
+
+function actualizarLista() {
+  
+}
+
